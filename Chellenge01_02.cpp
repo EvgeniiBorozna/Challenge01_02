c98e6b1c07711c51f4ccf45d3f53a6cf1bc2ee18
Evgenii Borozna <borozna@ya.ru>
2022-05-22 23:13:07 +0300

    1

---
+++ Chellenge01_02.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <random>
+#include <fstream>
+#include <string>
+
+int main(int argc, char* argv[])
+{
+    char cMove;
+    int iSec;
+    char cXOR;
+    cMove = *argv[3];
+    std::ofstream fRandKey;
+    fRandKey.open("file_random.txt", std::ios::out);
+    std::ofstream fUserKey;
+    fUserKey.open("file_user.txt", std::ios::out);
+    std::ifstream fOrigin;
+    fOrigin.open(argv[1], std::ios::in);
+
+    std::random_device random_device;   //random key generation
+    std::mt19937 generator(random_device());
+    std::uniform_int_distribution<> distribution(0, 1000);
+    iSec = distribution(generator);
+
+    char sX, cSec;
+    char* iUSec = argv[2];
+
+    while (!fOrigin.eof())
+    {
+        fOrigin >> sX;
+        cSec = sX ^ iSec;
+        fRandKey << cSec;
+        cSec = sX ^ (int)iUSec;
+        fUserKey << cSec;
+    }
+
+    fOrigin.close();
+    fRandKey.close();
+    fUserKey.close();
+    system("pause");
+    return 0;
+}
+
