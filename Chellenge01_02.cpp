060dea228e16cefde44deccc38999d71c49fe226
EvgeniiBorozna <102061594+EvgeniiBorozna@users.noreply.github.com>
2022-05-23 22:13:55 +0300

    add decription

--- Chellenge01_02.cpp
+++ Chellenge01_02.cpp
@@ -1,4 +1,4 @@
-// Parameters: [source_file] [key] [shift]
+// Parameters: [source_file] [key] [shift] [mode: e(encrypt) d(decrypt)]
 
 #include <iostream>
 #include <random>
@@ -7,52 +7,56 @@
 
 int main(int argc, char* argv[])
 {
-    int iSec;
-    int iShift = (int)*argv[3] - 48; //Shift value
-    std::ofstream fRandKey;
-    fRandKey.open("file_random.txt", std::ios::out); // File encrypted random key 
-    std::ofstream fUserKey;
-    fUserKey.open("file_user.txt", std::ios::out); // File encrypted user defined key
-    std::ifstream fOrigin;
-    fOrigin.open(argv[1], std::ios::in);
-
-    std::random_device random_device;   //random key generation
-    std::mt19937 generator(random_device());
-    std::uniform_int_distribution<> distribution(0, 1000);
-    iSec = distribution(generator);
-
-    char sX, sX1, cSec;
+    char cMode = *argv[4];
     char* iUSec = argv[2];
-    int iKeySize = strlen(iUSec);
- /*   for (int e = 0; e < iKeySize; e++) {
-        std::cout << iUSec[e];
-    }*/
+    char sX, sX1, cSec;
+    std::ofstream fRandKey;
+    std::ofstream fUserKey;
+    std::ifstream fOrigin;
 
-    int e = 0, s = 1;
-    while (!fOrigin.eof())
-    {
-        fOrigin >> sX;
-        cSec = sX ^ iSec;   //XOR with random key
-        fRandKey << cSec;
-        cSec = sX ^ iUSec[e]; // XOR with user key
-        if (e < (iKeySize - 1)) e++; else e = 0;
-        if (s < iShift) s++; else { // Moving every [iShift] symbol
-            s = 1;
-            fOrigin >> sX1;
+    if (cMode == 'e') {
+        int iSec;
+        int iShift = (int)*argv[3] - 48; //Shift value
+        fRandKey.open("file_random.txt", std::ios::out); // File encrypted random key 
+        fUserKey.open("file_user.txt", std::ios::out); // File encrypted user defined key
+        fOrigin.open(argv[1], std::ios::in);
+
+        std::random_device random_device;   //random key generation
+        std::mt19937 generator(random_device());
+        std::uniform_int_distribution<> distribution(0, 1000);
+        iSec = distribution(generator);
+        
+        int iKeySize = strlen(iUSec);
+
+        int e = 0, s = 1;
+        while (!fOrigin.eof())
+        {
             fOrigin >> sX;
-            cSec = sX ^ iSec;
+            cSec = sX ^ iSec;   //XOR with random key
             fRandKey << cSec;
-            cSec = sX ^ iUSec[e];
+            cSec = sX ^ iUSec[e]; // XOR with user key
             if (e < (iKeySize - 1)) e++; else e = 0;
+            if (s < iShift) s++; else { // Moving every [iShift] symbol
+                s = 1;
+                fOrigin >> sX1;
+                fOrigin >> sX;
+                cSec = sX ^ iSec;
+                fRandKey << cSec;
+                cSec = sX ^ iUSec[e];
+                if (e < (iKeySize - 1)) e++; else e = 0;
+                fUserKey << cSec;
+                cSec = sX1 ^ iSec;
+                fRandKey << cSec;
+                cSec = sX1 ^ iUSec[e];
+                if (e < (iKeySize - 1)) e++; else e = 0;
+            }
             fUserKey << cSec;
-            cSec = sX1 ^ iSec;
-            fRandKey << cSec;
-            cSec = sX1 ^ iUSec[e];
-            if (e < (iKeySize - 1)) e++; else e = 0;
         }
-        fUserKey << cSec;
     }
-
+    if (cMode == 'd') {
+        fUserKey.open("file_user_decr.txt", std::ios::out); // File decrypted by user key
+        fOrigin.open(argv[1], std::ios::in);
+    }
     fOrigin.close();
     fRandKey.close();
     fUserKey.close();
